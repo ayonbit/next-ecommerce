75cd9b0ed4f684c449179d8e31aba09ad18bc988
Ayon <suprovatbit@gmail.com>
2024-10-14 19:37:28 +0600

    Flter ,  List

--- src/app/list/page.jsx
+++ src/app/list/page.jsx
@@ -1,5 +1,36 @@
+//Dependencies
+import Image from "next/image";
+import Filter from "../components/Filter";
+import ProductList from "../components/ProductList";
+//List Page
 const ListPage = () => {
-  return <div>ListPage</div>;
+  return (
+    <div className="px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative">
+      {/* CAMPAIGN */}
+      <div className="hidden bg-pink-50 px-4 sm:flex justify-between h-64">
+        <div className="w-2/3 flex flex-col items-center justify-center gap-8">
+          <h1 className="text-4xl font-semibold leading-[48px] text-gray-700">
+            Grab Up to 50% off on Selected Products
+          </h1>
+          <button className="rounded-3xl bg-ayon  text-white w-max py-3 px-5 text-sm ">
+            {" "}
+            Buy Now
+          </button>
+        </div>
+        <div className="relative w-1/3">
+          <Image src="/woman.png" alt="women" fill className="object-contain" />
+        </div>
+      </div>
+      {/* Filter */}
+      <Filter />
+
+      {/* PRODUCT */}
+      <h1 className="mt-12 text-xl font-semibold">Product For you</h1>
+      <ProductList />
+      <h1 className="mt-12 text-xl font-semibold">Popular Products</h1>
+      <ProductList />
+    </div>
+  );
 };
 
 export default ListPage;
